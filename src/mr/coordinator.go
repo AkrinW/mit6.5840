6ae104848142067b03d4bbc48aecc412df6e794e
akarin <791972041@qq.com>
2024-11-18 19:51:34 +0800

    mapreduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,55 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+const (
+	TaskStatusIdle         = 0
+	TaskStatusProcessing   = 1
+	TaskStatusAccomplished = 2
+)
+
+const (
+	TaskTypeMap    = 0
+	TaskTypeReduce = 1
+)
+
+// Task任务数据
+type Task struct {
+	Status     int
+	TaskType   int
+	Filename   string
+	AssignTime time.Time
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	NumReduce               int
+	NumMap                  int
+	TaskLock                sync.Mutex
+	MapTasks                []Task
+	ReduceTasks             []Task
+	allMapTasksAccomplished bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +63,102 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
+// 分配任务的 RPC 处理函数
+func (c *Coordinator) AssignTask(args *ExampleArgs, reply *Task) error {
+	c.TaskLock.Lock()
+	defer c.TaskLock.Unlock()
+
+	// 检查是否有未完成的 Map 任务
+	if !c.allMapTasksAccomplished {
+		for i := range c.MapTasks {
+			task := &c.MapTasks[i]
+			if task.Status == TaskStatusIdle || (task.Status == TaskStatusProcessing && time.Since(task.AssignTime) > 10*time.Second) {
+				task.Status = TaskStatusProcessing
+				task.AssignTime = time.Now()
+				*reply = *task
+				return nil
+			}
+		}
+		c.allMapTasksAccomplished = c.checkAllTasksCompleted(TaskTypeMap)
+	}
+
+	// 如果 Map 任务都完成了，分配 Reduce 任务
+	if c.allMapTasksAccomplished {
+		for i := range c.ReduceTasks {
+			task := &c.ReduceTasks[i]
+			if task.Status == TaskStatusIdle || (task.Status == TaskStatusProcessing && time.Since(task.AssignTime) > 10*time.Second) {
+				task.Status = TaskStatusProcessing
+				task.AssignTime = time.Now()
+				*reply = *task
+				return nil
+			}
+		}
+	}
+
+	// 没有任务可分配
+	reply.Status = TaskStatusAccomplished
+	return nil
+}
+
+// 检查是否所有指定类型的任务已完成
+func (c *Coordinator) checkAllTasksCompleted(taskType int) bool {
+	var tasks []Task
+	if taskType == TaskTypeMap {
+		tasks = c.MapTasks
+	} else {
+		tasks = c.ReduceTasks
+	}
+
+	for _, task := range tasks {
+		if task.Status != TaskStatusAccomplished {
+			return false
+		}
+	}
+	return true
+}
+
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
+	c.TaskLock.Lock()
+	defer c.TaskLock.Unlock()
 
+	ret = c.allMapTasksAccomplished && c.checkAllTasksCompleted(TaskTypeReduce)
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		NumReduce:   nReduce,
+		NumMap:      len(files),
+		MapTasks:    make([]Task, len(files)),
+		ReduceTasks: make([]Task, nReduce),
+	}
 
 	// Your code here.
+	// 初始化 Map 任务
+	for i, filename := range files {
+		c.MapTasks[i] = Task{
+			Status:   TaskStatusIdle,
+			TaskType: TaskTypeMap,
+			Filename: filename,
+		}
+	}
 
+	// 初始化 Reduce 任务
+	for i := 0; i < nReduce; i++ {
+		c.ReduceTasks[i] = Task{
+			Status:   TaskStatusIdle,
+			TaskType: TaskTypeReduce,
+		}
+	}
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,31 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"time"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -35,14 +33,39 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	for {
+		// 向 Coordinator 请求任务
+		task := Task{}
+		if !call("Coordinator.AssignTask", &ExampleArgs{}, &task) {
+			fmt.Println("Failed to request task from Coordinator.")
+			return
+		}
 
+		// 检查是否任务已完成
+		if task.Status == TaskStatusAccomplished {
+			fmt.Println("All tasks are completed.")
+			break
+		}
+
+		// 根据任务类型执行相应的 Map 或 Reduce 操作
+		switch task.TaskType {
+		case TaskTypeMap:
+			handleMapTask(&task, mapf)
+		case TaskTypeReduce:
+			handleReduceTask(&task, reducef)
+		}
+
+		// 通知 Coordinator 当前任务已完成
+		call("Coordinator.TaskDone", &task, &ExampleReply{})
+
+		// 防止 Worker 过于频繁请求任务，适当等待
+		time.Sleep(time.Second)
+	}
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +90,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
@@ -88,4 +109,88 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+// 处理 Map 任务
+func handleMapTask(task *Task, mapf func(string, string) []KeyValue) {
+	// 读取输入文件内容
+	file, err := os.Open(task.Filename)
+	if err != nil {
+		log.Fatalf("Cannot open %v", task.Filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Cannot read %v", task.Filename)
+	}
+	file.Close()
+
+	// 执行 Map 函数
+	kva := mapf(task.Filename, string(content))
+
+	// 创建中间文件，分区写入
+	partitionFiles := make(map[int]*os.File)
+	encoders := make(map[int]*json.Encoder)
+
+	for _, kv := range kva {
+		// 计算分区号
+		partition := ihash(kv.Key) % task.NumReduce
+
+		// 打开或创建分区文件
+		if _, ok := partitionFiles[partition]; !ok {
+			filename := fmt.Sprintf("mr-%d-%d", task.TaskID, partition)
+			partitionFiles[partition], err = os.Create(filename)
+			if err != nil {
+				log.Fatalf("Cannot create file %v", filename)
+			}
+			encoders[partition] = json.NewEncoder(partitionFiles[partition])
+		}
+
+		// 写入分区文件
+		err := encoders[partition].Encode(&kv)
+		if err != nil {
+			log.Fatalf("Cannot encode key-value %v", kv)
+		}
+	}
+
+	// 关闭所有分区文件
+	for _, file := range partitionFiles {
+		file.Close()
+	}
+}
+
+// 处理 Reduce 任务
+func handleReduceTask(task *Task, reducef func(string, []string) string) {
+	// 收集和读取分区文件
+	kvMap := make(map[string][]string)
+	for i := 0; i < task.NumMap; i++ {
+		filename := fmt.Sprintf("mr-%d-%d", i, task.TaskID)
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("Cannot open %v", filename)
+		}
+		dec := json.NewDecoder(file)
+
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kvMap[kv.Key] = append(kvMap[kv.Key], kv.Value)
+		}
+		file.Close()
+	}
+
+	// 执行 Reduce 函数
+	oname := fmt.Sprintf("mr-out-%d", task.TaskID)
+	ofile, err := os.Create(oname)
+	if err != nil {
+		log.Fatalf("Cannot create %v", oname)
+	}
+
+	for key, values := range kvMap {
+		output := reducef(key, values)
+		fmt.Fprintf(ofile, "%v %v\n", key, output)
+	}
+
+	ofile.Close()
 }
